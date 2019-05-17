9737c5053d7214b5e1ffa492a78484ce197be8cb
Eddy Petrișor <eddy.petrisor@gmail.com>
2019-05-18 01:12:51 +0300

    Done assignment0 phase 4 - Rust blinky
    Signed-off-by: Eddy Petrișor <eddy.petrisor@gmail.com>

--- phase4/src/lib.rs
+++ phase4/src/lib.rs
@@ -19,8 +19,66 @@ fn spin_sleep_ms(ms: usize) {
     }
 }
 
+fn set_gpio_10_19_pin_as_out(pin: u8) -> Option<u8> {
+    let shift = if pin < 10 {
+        pin % 10
+    } else {
+        return None;
+    };
+
+    // let shift = pin % 10;
+    unsafe {
+        let old_val = GPIO_FSEL1.read_volatile() & (!(0b111 << shift));
+        GPIO_FSEL1.write_volatile(old_val | (0b001 << shift));
+    }
+
+    Some(shift)
+}
+
+fn set_gpio(gpio_n: u8) -> Option<u8> {
+    if gpio_n < 32u8 {
+        unsafe {
+            GPIO_SET0.write_volatile(1 << gpio_n);
+
+            Some(gpio_n)
+        }
+    } else {
+        None
+    }
+}
+
+fn clr_gpio(gpio_n: u8) -> Option<u8> {
+    if gpio_n < 32u8 {
+        unsafe {
+            GPIO_CLR0.write_volatile(1 << gpio_n);
+
+            Some(gpio_n)
+        }
+    } else {
+        None
+    }
+}
+
+
 #[no_mangle]
 pub unsafe extern "C" fn kmain() {
     // FIXME: STEP 1: Set GPIO Pin 16 as output.
+    if let None = set_gpio_10_19_pin_as_out(16) { panic!() };
     // FIXME: STEP 2: Continuously set and clear GPIO 16.
+    loop {
+        if let Some(_offset) = set_gpio(16u8) {
+
+        } else {
+            loop {}
+        };
+        spin_sleep_ms(500usize);
+
+        if let Some(_offset) = clr_gpio(16u8) {
+
+        } else {
+            loop {}
+        };
+        spin_sleep_ms(500usize);
+
+    }
 }
